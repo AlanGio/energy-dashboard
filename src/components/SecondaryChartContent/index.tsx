d58b5390fbfbdc37ff8b7475bfc67d4c6b816d2f
Alan <alan.gio@gmail.com>
2023-07-15 20:59:29 -0300

    Adding filter buttons

--- src/components/SecondaryChartContent/index.tsx
+++ src/components/SecondaryChartContent/index.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   FormControl,
   Grid,
   MenuItem,
@@ -34,32 +35,85 @@ export const SecondaryChartContent = () => {
     >
       <Grid container>
         <Grid item xs={12} sm={12}>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box>
             <Typography variant="h5" sx={{ fontWeight: 600, pt: 4 }}>
               Detailed Usage Data
             </Typography>
-            <Box>
-              <Typography variant="subtitle1" sx={{ fontWeight: 600, pt: 3 }}>
-                Reporting Period
-              </Typography>
-              <FormControl fullWidth sx={{ mb: 6 }}>
-                <Select
-                  value={reportingPeriod}
-                  onChange={handleChange}
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                pt: 1
+              }}
+            >
+              <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+                Filters
+                <Button
                   size="small"
+                  variant="secondarydark"
+                  sx={{
+                    textTransform: 'none',
+                    minWidth: 'auto',
+                    fontSize: 14,
+                    fontWeight: 400,
+                    letterSpacing: 0.5,
+                    backgroundColor: color.Primary500
+                  }}
                 >
-                  <MenuItem value="11-2022" selected>
-                    November 2022
-                  </MenuItem>
-                  <MenuItem value="12-2022">December 2022</MenuItem>
-                  <MenuItem value="01-2023">Juanary 2023</MenuItem>
-                  <MenuItem value="02-2023">February 2023</MenuItem>
-                  <MenuItem value="03-2023">March 2023</MenuItem>
-                  <MenuItem value="0411-2023">April 2023</MenuItem>
-                  <MenuItem value="05-2023">May 2023</MenuItem>
-                  <MenuItem value="06-2023">June 2023</MenuItem>
-                </Select>
-              </FormControl>
+                  Month
+                </Button>
+                <Button
+                  size="small"
+                  variant="secondary"
+                  sx={{
+                    textTransform: 'none',
+                    minWidth: 'auto',
+                    fontSize: 14,
+                    fontWeight: 400,
+                    letterSpacing: 0.5
+                  }}
+                >
+                  Week
+                </Button>
+                <Button
+                  size="small"
+                  variant="secondary"
+                  sx={{
+                    textTransform: 'none',
+                    minWidth: 'auto',
+                    fontSize: 14,
+                    fontWeight: 400,
+                    letterSpacing: 0.5
+                  }}
+                >
+                  Day
+                </Button>
+              </Box>
+              <Box>
+                <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+                  Reporting Period
+                </Typography>
+
+                <FormControl fullWidth sx={{ mb: 6 }}>
+                  <Select
+                    value={reportingPeriod}
+                    onChange={handleChange}
+                    size="small"
+                  >
+                    <MenuItem value="11-2022" selected>
+                      November 2022
+                    </MenuItem>
+                    <MenuItem value="12-2022">December 2022</MenuItem>
+                    <MenuItem value="01-2023">Juanary 2023</MenuItem>
+                    <MenuItem value="02-2023">February 2023</MenuItem>
+                    <MenuItem value="03-2023">March 2023</MenuItem>
+                    <MenuItem value="0411-2023">April 2023</MenuItem>
+                    <MenuItem value="05-2023">May 2023</MenuItem>
+                    <MenuItem value="06-2023">June 2023</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
             </Box>
           </Box>
         </Grid>

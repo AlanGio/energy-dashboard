9f06222160892fd2a5c8658059bec3883204fade
Alan <alan.gio@gmail.com>
2023-07-17 13:14:10 -0300

    Change Swcondary for area

--- src/components/TinyLineChart/index.tsx
+++ src/components/TinyLineChart/index.tsx
@@ -1,5 +1,5 @@
 import { SxProps, Box } from '@mui/material';
-import { LineChart, Line, ResponsiveContainer } from 'recharts';
+import { ResponsiveContainer, Area, ComposedChart } from 'recharts';
 
 export const TinyLineChart = ({
   sx,
@@ -11,14 +11,15 @@ export const TinyLineChart = ({
   return (
     <Box sx={sx}>
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart width={300} height={100} data={data}>
-          <Line
+        <ComposedChart width={300} height={100} data={data}>
+          <Area
             type="monotone"
             dataKey="usage"
+            fill="#8884d8"
             stroke="#8884d8"
-            strokeWidth={2}
+            name="Temperature"
           />
-        </LineChart>
+        </ComposedChart>
       </ResponsiveContainer>
     </Box>
   );

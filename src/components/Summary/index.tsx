1ff712200b0df9552cb19671b338204c2f5bac1a
Alan <alan.gio@gmail.com>
2023-07-28 10:16:28 -0300

    Update index.tsx

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,87 +3,115 @@ import { Box, Grid, Typography } from '@mui/material';
 
 export const Summary = ({ serviceData }) => {
   return (
-    <>
-      <Grid item xs={12} sm={6}>
-        <Box
-          sx={{
-            pr: [1, 4],
-            pb: [2, 4],
-            borderBottom: `1px solid ${color.TransparenciesSecondaryMain25}`,
-            borderRight: `1px solid ${color.TransparenciesSecondaryMain25}`
-          }}
-        >
-          <Typography
-            variant="h5"
+    <Box sx={{ width: '100%' }}>
+      <Grid container>
+        <Grid item xs={6} sm={6}>
+          <Box
             sx={{
-              fontWeight: 600,
-              mb: [1, 2],
-              fontSize: [16, 18]
+              pr: [1, 4],
+              pb: [2, 4],
+              borderBottom: `1px solid ${color.TransparenciesSecondaryMain25}`,
+              borderRight: `1px solid ${color.TransparenciesSecondaryMain25}`
             }}
           >
-            {serviceData[0].name}
-          </Typography>
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: 600,
+                mb: [1, 2],
+                fontSize: [16, 18]
+              }}
+            >
+              {serviceData[0].name}
+            </Typography>
 
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            {serviceData[0].values.map((item) => (
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center'
-                }}
-              >
-                <Typography variant="h3" sx={{ fontSize: [26, 36] }}>
-                  {item.value} {item.symbol}
-                </Typography>
-                <Typography variant="body2" sx={{ textTransform: 'uppercase' }}>
-                  {item.name}
-                </Typography>
-              </Box>
-            ))}
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: ['column', 'row'],
+                justifyContent: 'space-between'
+              }}
+            >
+              {serviceData[0].values.map((item) => (
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: ['flex-start', 'center'],
+                    pt: [1, 0]
+                  }}
+                >
+                  <Typography
+                    variant="h3"
+                    sx={{ fontSize: [17, 36], fontWeight: [600, 400] }}
+                  >
+                    {item.value} {item.symbol}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{ textTransform: ['none', 'uppercase'] }}
+                  >
+                    {item.name}
+                  </Typography>
+                </Box>
+              ))}
+            </Box>
           </Box>
-        </Box>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <Box
-          sx={{
-            pt: [2, 0],
-            pl: [0, 4],
-            pb: [2, 4],
-            borderBottom: `1px solid ${color.TransparenciesSecondaryMain25}`
-          }}
-        >
-          <Typography
-            variant="h5"
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <Box
             sx={{
-              fontWeight: 600,
-              mb: [1, 2],
-              fontSize: [16, 18]
+              pt: 0,
+              pl: [2, 4],
+              pb: [2, 4],
+              borderBottom: `1px solid ${color.TransparenciesSecondaryMain25}`
             }}
           >
-            {serviceData[1].name}
-          </Typography>
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: 600,
+                mb: [1, 2],
+                fontSize: [16, 18]
+              }}
+            >
+              {serviceData[1].name}
+            </Typography>
 
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            {serviceData[1].values.map((item) => (
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center'
-                }}
-              >
-                <Typography variant="h3" sx={{ fontSize: [26, 36] }}>
-                  {item.value} {item.symbol}
-                </Typography>
-                <Typography variant="body2" sx={{ textTransform: 'uppercase' }}>
-                  {item.name}
-                </Typography>
-              </Box>
-            ))}
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: ['column', 'row'],
+                justifyContent: 'space-between'
+              }}
+            >
+              {serviceData[1].values.map((item) => (
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: ['flex-start', 'center'],
+                    pt: [1, 0]
+                  }}
+                >
+                  <Typography
+                    variant="h3"
+                    sx={{ fontSize: [17, 36], fontWeight: [600, 400] }}
+                  >
+                    {item.value} {item.symbol}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{ textTransform: ['none', 'uppercase'] }}
+                  >
+                    {item.name}
+                  </Typography>
+                </Box>
+              ))}
+            </Box>
           </Box>
-        </Box>
+        </Grid>
       </Grid>
-    </>
+    </Box>
   );
 };

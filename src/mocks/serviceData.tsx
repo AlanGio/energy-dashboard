bb68a4b59fe79d4205c2138f644b67ef2f0015f5
Alan <alan.gio@gmail.com>
2023-07-28 10:17:02 -0300

    Update serviceData.tsx

--- src/mocks/serviceData.tsx
+++ src/mocks/serviceData.tsx
@@ -1,49 +1,49 @@
 export const serviceData = {
   electric: [
     {
-      name: 'Month-to-Date',
+      name: 'This Momth',
       values: [
-        { name: 'TOTAL ELECTRIC USAGE', value: '100,396', symbol: 'kWh' },
-        { name: 'PEAK DEMAND', value: '90.6', symbol: 'kW' }
+        { name: 'Total Electric Usage', value: '100,396', symbol: 'kWh' },
+        { name: 'Peak Demand', value: '90.6', symbol: 'kW' }
       ]
     },
     {
-      name: 'Year-to-Date',
+      name: 'This Year',
       values: [
-        { name: 'TOTAL ELECTRIC USAGE', value: '305,395', symbol: 'kWh' },
-        { name: 'PEAK DEMAND', value: '125.6', symbol: 'kW' }
+        { name: 'Total Electric Usage', value: '305,395', symbol: 'kWh' },
+        { name: 'Peak Demand', value: '125.6', symbol: 'kW' }
       ]
     }
   ],
   gas: [
     {
-      name: 'Month-to-Date',
+      name: 'This Month',
       values: [
-        { name: 'TOTAL ELECTRIC USAGE', value: '89,236', symbol: 'cal' },
-        { name: 'PEAK DEMAND', value: '10.8', symbol: 'cal' }
+        { name: 'Total Electric Usage', value: '89,236', symbol: 'cal' },
+        { name: 'Peak Demand', value: '10.8', symbol: 'cal' }
       ]
     },
     {
-      name: 'Year-to-Date',
+      name: 'This Year',
       values: [
-        { name: 'TOTAL ELECTRIC USAGE', value: '15,323', symbol: 'cal' },
-        { name: 'PEAK DEMAND', value: '130.2', symbol: 'cal' }
+        { name: 'Total Electric Usage', value: '15,323', symbol: 'cal' },
+        { name: 'Peak Demand', value: '130.2', symbol: 'cal' }
       ]
     }
   ],
   water: [
     {
-      name: 'Month-to-Date',
+      name: 'This Month',
       values: [
-        { name: 'TOTAL ELECTRIC USAGE', value: '40,893', symbol: 'gal' },
-        { name: 'PEAK DEMAND', value: '1.8', symbol: 'gal' }
+        { name: 'Total Electric Usage', value: '40,893', symbol: 'gal' },
+        { name: 'Peak Demand', value: '1.8', symbol: 'gal' }
       ]
     },
     {
-      name: 'Year-to-Date',
+      name: 'This Year',
       values: [
-        { name: 'TOTAL ELECTRIC USAGE', value: '20,122', symbol: 'gal' },
-        { name: 'PEAK DEMAND', value: '456.2', symbol: 'gal' }
+        { name: 'Total Electric Usage', value: '20,122', symbol: 'gal' },
+        { name: 'Peak Demand', value: '456.2', symbol: 'gal' }
       ]
     }
   ]
